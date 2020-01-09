b825bf283b2c033f6937aba42f23e59f5ea1cf23
hashiting <leepengqiu@gmail.com>
2020-01-10 00:55:47 +0100

    simple version

--- FFT_MPI.c
+++ FFT_MPI.c
@@ -12,14 +12,14 @@
 
 #define PI 3.14159265
 #define bigN 8192 //Problem Size
-#define howmanytimesavg 5 //How many times do I wanna run for the AVG?
+#define howmanytimesavg 5 
 
 int main()
 {
 	int my_rank,comm_sz;
 	MPI_Init(NULL,NULL); //start MPI
-	MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);   ///how many processes are we using?
-	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);   //which process is this?
+	MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);   
+	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);   
 
 	double start,finish;
 	double avgtime = 0;
@@ -29,13 +29,13 @@ int main()
 	{
 		outfile = fopen("result.txt", "w"); //open from current directory
 	}
-	for(h = 0; h < howmanytimesavg; h++) //loop to run multiple times for AVG time.
+	for(h = 0; h < howmanytimesavg; h++) 
 	{
-		if(my_rank == 0) //If it's process 0 starts timer
+		if(my_rank == 0) 
 		{	
 			start = MPI_Wtime();
 		}
-		int i,k,n,j; //Basic loop variables
+		int i,k,n,j; 
 
 		double complex evenpart[(bigN / comm_sz / 2)]; //array to save the data for EVENHALF
 		double complex oddpart[(bigN / comm_sz / 2)]; //array to save the data for ODDHALF
@@ -46,18 +46,17 @@ int main()
 		
 		double subtable[(bigN / comm_sz)][3]; //Each process owns a subtable from the table below 
 		
-		double table[bigN][3] = //TABLE of numbers to use
-							{
-							 0,3.6,2.6, //n, Real,Imaginary CREATES TABLE
-							 1,2.9,6.3,
-							 2,5.6,4.0,
-							 3,4.8,9.1,
-							 4,3.3,0.4,
-							 5,5.9,4.8,
-							 6,5.0,2.6,
-							 7,4.3,4.1,
-							 };
-			if(bigN > 8)  //Everything after row 8 is all 0's
+		double table[bigN][3] = {
+									0,3.6,2.6, 
+									1,2.9,6.3,
+									2,5.6,4.0,
+									3,4.8,9.1,
+									4,3.3,0.4,
+									5,5.9,4.8,
+									6,5.0,2.6,
+									7,4.3,4.1,
+								};
+			if(bigN > 8)
 			{
 				for(i = 8; i < bigN; i++)
 				{
@@ -68,11 +67,13 @@ int main()
 					}
 				}
 			}
-		int sendandrecvct = (bigN / comm_sz) * 3; //how much to send and recieve??
+		
+
+		int sendandrecvct = (bigN / comm_sz) * 3; 
 		MPI_Scatter(table,sendandrecvct,MPI_DOUBLE,subtable,sendandrecvct,MPI_DOUBLE,0,MPI_COMM_WORLD); //scatter the table to subtables
 		for (k = 0; k < bigN / 2; k++) //K coeffiencet Loop 
 		{
-					/* Variables used for the computation */
+					
 			double sumrealeven = 0.0; //sum of real numbers for even
 			double sumimageven = 0.0; //sum of imaginary numbers for even
 			double sumrealodd = 0.0; //sum of real numbers for odd
@@ -88,33 +89,34 @@ int main()
 				double realeven = subtable[2*i][1]; //Access table for real number at spot 2i
 				double complex imaginaryeven = subtable[2*i][2]; //Access table for imaginary number at spot 2i
 				double complex componeeven = (realeven + imaginaryeven * I); //Create the first component from table
-				if(my_rank == 0) //if proc 0, dont use shiftevenonnonzeroP
+				if(my_rank == 0) 
 				{
 					factoreven = ((2*PI)*((2*i)*k))/bigN; //Calculates the even factor for Cos() and Sin()										
-								//   *********Reduces computational time*********
+								
 				}
-				else //use shiftevenonnonzeroP
+				else
 				{
 					factoreven = ((2*PI)*((shiftevenonnonzeroP)*k))/bigN; //Calculates the even factor for Cos() and Sin()										
-								//   *********Reduces computational time*********
+								
 				}
 				double complex comptwoeven = (cos(factoreven) - (sin(factoreven)*I)); //Create the second component
 				
 				evenpart[i] = (componeeven * comptwoeven); //store in the evenpart array
 				
+
 				/* -------- ODD PART -------- */
 				double realodd = subtable[2*i + 1][1]; //Access table for real number at spot 2i+1
 				double complex imaginaryodd = subtable[2*i + 1][2]; //Access table for imaginary number at spot 2i+1
 				double complex componeodd = (realodd + imaginaryodd * I); //Create the first component from table
-				if (my_rank == 0)//if proc 0, dont use shiftoddonnonzeroP
+				if (my_rank == 0)
 				{
 					factorodd = ((2*PI)*((2*i+1)*k))/bigN;//Calculates the odd factor for Cos() and Sin()										
-								// *********Reduces computational time*********	
+									
 				}
-				else //use shiftoddonnonzeroP
+				else 
 				{
 					factorodd = ((2*PI)*((shiftoddonnonzeroP)*k))/bigN;//Calculates the odd factor for Cos() and Sin()										
-								// *********Reduces computational time*********
+								
 				}
 							
 				double complex comptwoodd = (cos(factorodd) - (sin(factorodd)*I));//Create the second component
@@ -122,7 +124,8 @@ int main()
 				oddpart[i] = (componeodd * comptwoodd); //store in the oddpart array
 				
 			}
-			/*Process ZERO gathers the even and odd part arrays and creates a evenpartmaster and oddpartmaster array*/
+			
+
 			MPI_Gather(evenpart,(bigN / comm_sz / 2),MPI_DOUBLE_COMPLEX,evenpartmaster,(bigN / comm_sz / 2), MPI_DOUBLE_COMPLEX,0,MPI_COMM_WORLD);
 			MPI_Gather(oddpart,(bigN / comm_sz / 2),MPI_DOUBLE_COMPLEX,oddpartmaster,(bigN / comm_sz / 2), MPI_DOUBLE_COMPLEX,0,MPI_COMM_WORLD);
 
@@ -139,35 +142,36 @@ int main()
 				storeKsumimag[k]  = sumimageven + sumimagodd; //add the calculated imaginary from even and odd
 				storeKsumreal[k + bigN/2] = sumrealeven - sumrealodd; //ABUSE symmetry Xkreal + N/2 = Evenk - OddK
 				storeKsumimag[k + bigN/2] = sumimageven - sumimagodd; //ABUSE symmetry Xkimag + N/2 = Evenk - OddK
-				if(k < 4) //8 point fft
+
+				if(k < 4) 
 				{
 					if(k == 0)
 					{
 						fprintf(outfile," \n\n TOTAL PROCESSED SAMPLES : %d\n",bigN);
 					}
-					fprintf(outfile,"================================\n");
+					//fprintf(outfile,"================================\n");
 					fprintf(outfile,"XR[%d]: %.4f XI[%d]: %.4f \n",k,storeKsumreal[k],k,storeKsumimag[k]);
-					fprintf(outfile,"================================\n");
+					//fprintf(outfile,"================================\n");
                     fprintf(outfile,"XR[%d]: %.4f XI[%d]: %.4f \n",k + bigN/2,storeKsumreal[k + bigN/2],k + bigN/2,storeKsumimag[k + bigN/2]);
-					fprintf(outfile,"================================\n");
+					//fprintf(outfile,"================================\n");
 				}
 			}
 		}
 		if(my_rank == 0)
 		{
-			finish = MPI_Wtime(); //stop timer
-			double timeElapsed = finish-start; //Time for that iteration
-			avgtime = avgtime + timeElapsed; //AVG the time 
+			finish = MPI_Wtime();
+			double timeElapsed = finish-start; 
+			avgtime = avgtime + timeElapsed; 
 			fprintf(outfile,"Time Elaspsed on Iteration %d: %f Seconds\n", (h+1),timeElapsed);
 		}
 	}
 	if(my_rank == 0)
 	{
-		avgtime = avgtime / howmanytimesavg; //get avg time
+		avgtime = avgtime / howmanytimesavg; 
 		fprintf(outfile,"\nAverage Time Elaspsed: %f Seconds", avgtime);
-		fclose(outfile); //CLOSE file ONLY proc 0 can.
+		fclose(outfile);
 	}
-	MPI_Barrier(MPI_COMM_WORLD); //wait to all proccesses to catch up before finalize
-	MPI_Finalize(); //End MPI
+	MPI_Barrier(MPI_COMM_WORLD); 
+	MPI_Finalize(); 
 	return 0;
 }
